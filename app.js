22adecca0645918be25e21f349cf3ad048103be7
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-13 10:16:35 +0800

    Remove unnecessary deps

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
 
 const index = require("./routes/index");
 const books = require("./routes/books");
@@ -9,7 +8,6 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", index);
 app.use("/books", books);
