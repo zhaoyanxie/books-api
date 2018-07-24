5967898677f276d318c736dec6e6ff081c7b6580
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-24 16:03:50 +0800

    Added try-catch wrapper

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const logger = require("morgan");
+const express = require('express');
+const logger = require('morgan');
 
-const index = require("./routes/index");
-const books = require("./routes/books.js");
-const authors = require("./routes/authors.js");
+const index = require('./routes/index');
+const books = require('./routes/books.js');
+const authors = require('./routes/authors.js');
 
 const app = express();
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 
-app.use("/", index);
-app.use("/books", books);
-app.use("/authors", authors);
+app.use('/', index);
+app.use('/books', books);
+app.use('/authors', authors);
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -1,43 +1,50 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Book = require("../models/book");
-const mongoose = require("mongoose");
+const Book = require('../models/book');
+const mongoose = require('mongoose');
 
+const tryWrapper = (asyncMiddleware) => {
+	try {
+		return async (req, res, next) => {
+			await asyncMiddleware(req, res, next);
+		};
+	} catch (error) {
+		next(error);
+	}
+};
 /* GET books listing. */
-router.get("/", async (req, res, next) => {
-  try {
-    const books = await Book.find().populate("author");
-    res.json(books);
-  } catch (error) {
-    next(error);
-  }
+router.get(
+	'/',
+	tryWrapper(async (req, res, next) => {
+		const books = await Book.find().populate('author');
+		res.json(books);
+	})
+);
+
+router.get('/:id', (req, res, next) => {
+	res.json({ message: `get book with id ${req.params.id}` });
 });
 
-router.get("/:id", (req, res, next) => {
-  res.json({ message: `get book with id ${req.params.id}` });
+router.post(
+	'/',
+	tryWrapper(async (req, res, next) => {
+		const newBook = new Book({
+			title: req.body.title,
+			author: req.body.author
+		});
+
+		await newBook.save();
+
+		res.status(201).json({ message: `created a new book successfully` });
+	})
+);
+
+router.put('/:id', (req, res, next) => {
+	res.json({ message: `update book with id ${req.params.id}` });
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    const newBook = new Book({
-      title: req.body.title,
-      author: req.body.author
-    });
-
-    await newBook.save();
-
-    res.status(201).json({ message: `created a new book successfully` });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", (req, res, next) => {
-  res.json({ message: `update book with id ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res, next) => {
-  res.json({ message: `delete book with id ${req.params.id}` });
+router.delete('/:id', (req, res, next) => {
+	res.json({ message: `delete book with id ${req.params.id}` });
 });
 
 module.exports = router;
