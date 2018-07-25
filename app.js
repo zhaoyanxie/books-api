cd3b8285aa993e554cb74dc6adf31d143277a13f
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-25 14:17:00 +0800

    Added express cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const index = require('./routes/index');
 const books = require('./routes/books.js');
@@ -8,6 +9,8 @@ const authors = require('./routes/authors.js');
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/', index);
 app.use('/books', books);
