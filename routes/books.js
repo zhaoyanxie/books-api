65b4b646362a9f4f6949a48cfb0f18ed40d000a3
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-25 15:03:32 +0800

    removed cors within books route

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
-const cors = require('cors');
 
-router.use(cors());
 const tryWrapper = (asyncMiddleware) => {
 	try {
 		return async (req, res, next) => {
